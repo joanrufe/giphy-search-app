38ca7246a778520644ec1d60885b4bdc7d47e877
Antonio Ruiz <stayforawhile@gmail.com>
2019-03-01 01:34:33 +0100

    Updates

---
+++ precache-manifest.cffbb531404577cf22e1beb59c0705b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "935f926b1225ef4178ce",
+    "url": "/giphy-search-app/static/js/runtime~main.935f926b.js"
+  },
+  {
+    "revision": "c5bb903245bfcf28e56e",
+    "url": "/giphy-search-app/static/js/main.c5bb9032.chunk.js"
+  },
+  {
+    "revision": "3c80092b0baffbadd3a7",
+    "url": "/giphy-search-app/static/js/2.3c80092b.chunk.js"
+  },
+  {
+    "revision": "c5bb903245bfcf28e56e",
+    "url": "/giphy-search-app/static/css/main.a2a2d9db.chunk.css"
+  },
+  {
+    "revision": "3c80092b0baffbadd3a7",
+    "url": "/giphy-search-app/static/css/2.03c2f43c.chunk.css"
+  },
+  {
+    "revision": "4186ae9ef754a27adb503aa975f0e3e8",
+    "url": "/giphy-search-app/index.html"
+  }
+];
